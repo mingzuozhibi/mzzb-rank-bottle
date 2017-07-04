a8fb6fae12650c9baf622d1a37b92760f50e1b2c
yakami <c360785655@gmail.com>
2017-07-04 16:37:54 +0800

    update rank

--- rank.sh
+++ rank.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while true; do
-  git add --all && git commit -m "update rank" && git push origin master
+  git add --all && git pull && git commit -m "update rank" && git push origin master
   sleep 30
 done
